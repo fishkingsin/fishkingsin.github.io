885c96595f24c69910d2f1c0a97bc5c2369707a2
James Kong <fishkingsin@gmail.com>
2013-08-21 11:10:54 +0800

    commit

--- js/three.canvas1.js
+++ js/three.canvas1.js
@@ -40,54 +40,7 @@ function init() {
 
 	container = document.getElementById('three');
 
-	// document.body.appendChild( container );
-
-	// var info = document.createElement( 'div' );
-	// info.style.position = 'absolute';
-	// info.style.top = '10px';
-	// info.style.width = '100%';
-	// info.style.textAlign = 'center';
-	// info.innerHTML = '<a href="http://threejs.org" target="_blank">three.js</a> canvas - interactive particles';
-	// container.appendChild( info );
-
-	// camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
-	// camera.position.set( 0, 300, 500 );
-
-	// scene = new THREE.Scene();
-
 	
-	// var PI2 = Math.PI * 2;
-	// 			var material = new THREE.ParticleCanvasMaterial( {
-
-	// 				color: 0xffffff,
-	// 				program: function ( context ) {
-
-	// 					context.beginPath();
-	// 					context.arc( 0, 0, 1, 0, PI2, true );
-	// 					context.closePath();
-	// 					context.fill();
-
-	// 				}
-
-	// 			} );
-
-	// 			for ( var i = 0; i < 1000; i ++ ) {
-
-	// 				particle = new THREE.Particle( material );
-	// 				particle.position.x = Math.random() * 2 - 1;
-	// 				particle.position.y = Math.random() * 2 - 1;
-	// 				particle.position.z = Math.random() * 2 - 1;
-	// 				particle.position.normalize();
-	// 				particle.position.multiplyScalar( Math.random() * 10 + 450 );
-	// 				scene.add( particle );
-
-	// 			}
-	// projector = new THREE.Projector();
-
-	// renderer = new THREE.CanvasRenderer();
-	// renderer.setSize( window.innerWidth, window.innerHeight );
-
-	// container.appendChild( renderer.domElement );
 
 	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
 	camera.position.z = 1000;
@@ -129,12 +82,6 @@ function init() {
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	container.appendChild( renderer.domElement );
 
-
-	// stats = new Stats();
-	// stats.domElement.style.position = 'absolute';
-	// stats.domElement.style.top = '0px';
-	// container.appendChild( stats.domElement );
-
 	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 
 	//
@@ -168,7 +115,6 @@ function animate() {
 	requestAnimationFrame( animate );
 
 	render();
-	// stats.update();
 
 }
 
@@ -181,42 +127,6 @@ function render() {
 
 	theta += 0.1;
 
-	/*camera.position.x = radius * Math.sin( THREE.Math.degToRad( theta ) );
-	camera.position.y = 0;//radius * Math.sin( THREE.Math.degToRad( theta ) );
-	camera.position.z = radius * Math.cos( THREE.Math.degToRad( theta ) );
-	camera.lookAt( scene.position );
-
-	// find intersections
-
-	camera.updateMatrixWorld();
-
-	// var vector = new THREE.Vector3( mouse.x, mouse.y, 0.5 );
-	// projector.unprojectVector( vector, camera );
-
-	// var raycaster = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
-
-	// var intersects = raycaster.intersectObjects( scene.children );
-
-	// if ( intersects.length > 0 ) {
-
-	// 	if ( INTERSECTED != intersects[ 0 ].object ) {
-
-	// 		if ( INTERSECTED ) INTERSECTED.material.program = programStroke;
-
-	// 		INTERSECTED = intersects[ 0 ].object;
-	// 		INTERSECTED.material.program = programFill;
-
-	// 	}
-
-	// } else {
-
-	// 	if ( INTERSECTED ) INTERSECTED.material.program = programStroke;
-
-	// 	INTERSECTED = null;
-
-	// }
-
-	renderer.render( scene, camera );*/
 	camera.position.x = radius * Math.sin( THREE.Math.degToRad( theta ) );
 	camera.position.y = 0;//radius * Math.sin( THREE.Math.degToRad( theta ) );
 	camera.position.z = radius * Math.cos( THREE.Math.degToRad( theta ) );
