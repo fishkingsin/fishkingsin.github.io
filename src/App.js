6973c7c4cf97b15284a3257ba601fc0a908d9f91
James Kong <104340270+jameskongchief@users.noreply.github.com>
2023-08-18 00:31:51 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Under Construction
-        </p>
-
-      </header>
+      </header>>
     </div>
   );
 }
